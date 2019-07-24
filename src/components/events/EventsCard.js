62de137de8d89a575c3dd1723e7df05330653002
Curt Cato <ccato829@gmail.com>
2019-07-24 13:02:12 -0500

    commit

--- src/components/events/EventsCard.js
+++ src/components/events/EventsCard.js
@@ -3,33 +3,48 @@ import "./events.css";
 // import APIManager from "../../module/APIManager";
 
 export default class EventsCard extends Component {
+  editButton = (
+    <button
+      type="button"
+      className="btn-success"
+      onClick={() => {
+        this.props.history.push(`/events/${this.props.event.id}/edit`);
+      }}
+    >
+      Edit
+    </button>
+  );
+
+  deleteButton = (
+    <button
+      onClick={() => this.props.deleteEvent(this.props.event.id)}
+      className="card-link"
+    >
+      Delete
+    </button>
+  );
+
   render() {
     return (
       <div key={this.props.event.id} className="card">
         <div className="card-body">
           <div class-name="card-title">
-            <h5>{this.props.event.title}</h5>
-            <h5>{this.props.event.date}</h5>
-            <h5>{this.props.event.time}</h5>
-            <h5>{this.props.event.location}</h5>
-
-            <button
-              type="button"
-              className="btn-success"
-              onClick={() => {
-                this.props.history.push(
-                  `/events/${this.props.event.id}/edit`);
-              }}
-            >
-              Edit
-            </button>
-            <button
-              onClick={() => this.props.deleteEvent(
-                this.props.event.id)}
-              className="card-link"
-            >
-              Delete
-            </button>
+            <h5>Event Title: {this.props.event.title}</h5>
+            <h5>Event Date: {this.props.event.date}</h5>
+            <h5>Event Time: {this.props.event.time}</h5>
+            <h5>Event Location: {this.props.event.location}</h5>
+            <div>
+              {parseInt(this.props.event.userId) ===
+              parseInt(sessionStorage.getItem("userId"))
+                ? this.editButton
+                : null}
+            </div>
+            <div>
+              {parseInt(this.props.event.userId) ===
+              parseInt(sessionStorage.getItem("userId"))
+                ? this.deleteButton
+                : null}
+            </div>
           </div>
         </div>
       </div>
